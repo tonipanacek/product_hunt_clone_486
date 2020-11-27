2ba1b57cc4ed3fd2ce813edfa35d39bd62794241
Toni Panacek <toni.panacek@gmail.com>
2020-11-27 09:30:17 +0100

    added product title and tagline

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
+  # before_action :set_product, only: [ :show, :update ]
+  skip_before_action :authenticate_user!, only: [ :index ]
+
   def index
     @products = Product.all
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
